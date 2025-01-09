978264662666148ac16c3a73edafc34329afe979
gunatit369 <adityakalmekh2112@gmail.com>
2025-01-09 18:49:20 +0530

    Updated socket code.

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -22,22 +22,25 @@ export const initializeSocket = (userId: string) => {
             reconnection: true,
             reconnectionAttempts: 5,
             reconnectionDelay: 2000,
+            reconnectionDelayMax: 5000,
             autoConnect: false, // Prevent automatic connection
             secure: true,
             withCredentials: true,
             path: '/socket.io/',
             timeout: 20000,
             forceNew: true,
-            extraHeaders: {
-                "User-Agent": "Socket-Client"
-            }
         });
 
         socket.on('connect', () => {
-            console.log('Connected with transport:', socket?.io?.engine?.transport?.name);
+            // console.log('Connected with transport:', socket?.io?.engine?.transport?.name);
+            console.log('Connected:', {
+                id: socket?.id,
+                transport: socket?.io?.engine?.transport?.name
+            });
         });
 
         socket.on('connect_error', (error) => {
+            console.log("In socket error");
             console.error('Connection error:', error);
         });
     
