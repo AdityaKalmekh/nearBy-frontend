8fe8df00c1e2048f5a29f9b9aeb65e3656221f64
gunatit369 <adityakalmekh2112@gmail.com>
2025-02-22 00:25:55 +0530

    Refactor home page with content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import HomeServicesSection from "./components/HomeServiceSelection";
 import Navigation from "./components/navbar/Navigation";
 
 export default function Home() {
   return (
-    <Navigation />
+    <>
+      <Navigation />
+      <HomeServicesSection />
+    </>
   );
 }
